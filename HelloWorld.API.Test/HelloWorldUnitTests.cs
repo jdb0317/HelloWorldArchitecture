e6fb383c4fd7791b512ab6babc46e8729109935b
jbrown <jonathanbrown0123@gmail.com>
2018-09-15 15:36:55 -0300

    Cleanup usings

---
+++ HelloWorld.API.Test/HelloWorldUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using HelloWorld.Domain.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld.Domain;
+using HelloWorld.Domain.Entity;
+using HelloWorld.API.Business;
+
+namespace HelloWorld.API.Test
+{
+    [TestClass]
+    public class HelloWorldUnitTests
+    {
+        [TestMethod]
+        public void WorkHanderReturnsMessage()
+        {
+            ReturnMessage expectedValue = new ReturnMessage() { MessageValue = Constants.HelloMessage };
+            Mock<IDataRepository> mockRepo = new Mock<IDataRepository>();
+            mockRepo.Setup(m => m.GetMessage()).Returns(expectedValue);
+
+            WorkHandler handler = new WorkHandler(mockRepo.Object);
+            var returnValue = handler.GetMessage();
+
+            Assert.IsNotNull(returnValue);
+            Assert.AreEqual(returnValue, expectedValue);
+            Assert.AreEqual(returnValue.MessageValue, Constants.HelloMessage);
+        }
+    }
+}
